7f5407d1a78afb884d20b4d83253573aabbc1e9a
Dean Gardiner <gardiner91@gmail.com>
2015-07-03 20:52:38 +1200

    Fixed `PlexAuthentication` reference in [app/scripts/controllers/login.js]

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -8,14 +8,14 @@
  * Controller of the configurationApp
  */
 angular.module('configurationApp')
-  .controller('LoginController', function(PlexAuthentication, $location, $scope) {
+  .controller('LoginController', function(PAuthentication, $location, $scope) {
     $scope.credentials = {
       username: null,
       password: null
     };
 
     $scope.submit = function() {
-      PlexAuthentication.login($scope.credentials).then(function() {
+      PAuthentication.login($scope.credentials).then(function() {
         $location.path($scope.$a.originalPath);
       }, function() {
         console.log('login error');
