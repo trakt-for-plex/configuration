95bfd7d01054a59a7f3de6e9092f4fc7f6a3151e
Dean Gardiner <gardiner91@gmail.com>
2015-07-03 20:53:49 +1200

    Basic server configuration view and "option" directive structure

--- app/scripts/controllers/configuration/server.js
+++ app/scripts/controllers/configuration/server.js
@@ -8,6 +8,42 @@
  * Controller of the configurationApp
  */
 angular.module('configurationApp')
-  .controller('ServerController', function () {
+  .controller('ServerController', function ($rootScope, $scope) {
+    $scope.groups = {};
 
+    function parseOptions(groups, options) {
+      for(var i = 0; i < options.length; ++i) {
+        var option = options[i],
+          groupName = option.group[0],
+          group = groups[groupName];
+
+        if(typeof group === 'undefined') {
+          group = groups[groupName] = {
+            name: groupName,
+            groups: {},
+
+            options: []
+          };
+        }
+
+        if(option.group.length === 1) {
+          group.options.push(option);
+        } else {
+          var o = angular.copy(option);
+
+          // Remove first group name
+          o.group.shift();
+
+          parseOptions(group.groups, [o]);
+        }
+      }
+    }
+
+    $rootScope.$s.call('preferences.list', []).then(function(result) {
+      $scope.groups = {};
+
+      parseOptions($scope.groups, result.options);
+
+      console.log($scope.groups);
+    });
   });
---
+++ app/scripts/directives/option.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('configurationApp')
+  .directive('coOption', function() {
+    return {
+      restrict: 'E',
+      scope: {
+        option: '=option'
+      },
+      template: '<ng-include src="getTemplateUrl()"/>',
+      controller: function($scope) {
+        var baseUrl = 'directives/option/',
+          templateMap = {
+            boolean:  'boolean.html',
+            enum:     'enum.html',
+            integer:  'integer.html',
+            string:   'string.html'
+          };
+
+        $scope.getTemplateUrl = function() {
+          return baseUrl + templateMap[$scope.option.type];
+        };
+      }
+    };
+  });
