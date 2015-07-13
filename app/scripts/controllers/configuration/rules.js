ec881ebb6d24e82b773cdd5cfe9880ee9af08875
Dean Gardiner <gardiner91@gmail.com>
2015-07-13 23:22:22 +1200

    Finished implementing rule re-ordering (move items, update priorities)
    TFPC-20 #done

--- app/scripts/controllers/configuration/rules.js
+++ app/scripts/controllers/configuration/rules.js
@@ -46,10 +46,42 @@ angular.module('configurationApp')
           from = $item.data('drag-from'),
           to = Array.prototype.indexOf.call(tbody.children, $item[0]);
 
+        // Update dragged item
         $item.removeClass(container.group.options.draggedClass).removeAttr("style");
+
+        // Update cursor
         $("body").removeClass(container.group.options.bodyClass);
 
-        console.log('onDrop', from, to);
+        // Retrieve collection
+        var $table = $item.parents('table'),
+            collectionName = $table.data('collection'),
+            collection = null;
+
+        if(collectionName === 'client') {
+          collection = $scope.client.rules;
+        } else if(collectionName === 'user') {
+          collection = $scope.user.rules;
+        } else {
+          console.warn('Unable to find collection with the name "%s"', collectionName);
+          return;
+        }
+
+        // Retrieve rule from `collection`
+        var item = collection[from];
+
+        if(typeof item === 'undefined') {
+          console.warn('Unable to retrieve item with index %s', from);
+          return;
+        }
+
+        // Move rule inside `collection`
+        collection.splice(from, 1);
+        collection.splice(to, 0, item);
+
+        // Update rule priorities
+        for(var i = 0; i < collection.length; ++i) {
+          collection[i].priority = i + 1;
+        }
       }
     });
 
