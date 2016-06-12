05005de9afe87610b5e6cf2efa8fc9d4d13ce349
Dean Gardiner <me@dgardiner.net>
2016-06-13 00:14:32 +1200

    Bumped minimum plugin version to 1.0.3.0

--- app/scripts/models/plex/server.js
+++ app/scripts/models/plex/server.js
@@ -3,7 +3,7 @@
 angular.module('configurationApp')
   .factory('PlexServer', function(PlexConnection, PlexConnectionManager, Utils, VersionUtil, $location, $q, $rootScope) {
     var identifier = 'com.plexapp.plugins.trakttv',
-        pluginVersionMinimum = '0.9.10.11',
+        pluginVersionMinimum = '1.0.3.0',
         target = 'MessageKit:Api';
 
     function parseErrorResponse(response) {
